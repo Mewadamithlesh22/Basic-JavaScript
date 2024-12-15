52f1c688dc16a249116c67fdde39c5710ac96980
Mewadamithlesh22 <0112it221034@gmail.com>
2024-12-15 15:08:27 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("mithlesh")
+let v = 10;
+let m = 20;
+console.log(v);
+console.log(m);
+c = 30
+console.log(c);
+/*
+In java Script we Avoid to use var key word to 
+declare any keyword we use let to place of var key word 
+*/
+
+
